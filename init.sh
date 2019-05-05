3a665f007c9b366a9ae848b1df1a14a3cdb68925
Michael Baumgarten <miba358@gmail.com>
2019-05-04 21:33:15 -0400

    fixed color formatting

--- init.sh
+++ init.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # init 
-# ====
-
+#
 # Purpose: To simplify the process of provisioning a new developer machine
-
+#
 # Method: Any tools, software or configurations that are commonly
 # needed may be offered by running this script. 
-
+#
 # - No software will be installed without the user directing the script to do so
 # - When this script is first run a series of prompts will allow the user to
 #   select which tools (and versions) should be installed
@@ -23,17 +22,19 @@
 # - The script will request the sudoers password in prompt and only use it for
 #   package manager operations
 
-# GLOBAL VARS
+################# GLOBAL VARS #################
 USERNAME=$USER
 HOSTNAME=$HOSTNAME
 DATE="$(date)"
 
+# PROMPT COLORS
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-RST='\033[0;33m'
-NC='\033[0m'
+ORANGE='\033[0;33m'
+NOCOLOR='\033[0m'
 
-# SETUP VARS (n) for don't install (y) for install
+################# FEATURE VARS ################
+# (n) for don't install (y) for install
 GIT='n'
 GIT_PROMPT='n'
 PYENV='n'
@@ -46,8 +47,12 @@ YARN='n'
 NPM='n'
 NODEJS='n'
 
-printf "${RST}"
+############## HELPER FUNCTIONS ###############
 
+# purpose: set a feature variable based on user input
+#
+# args: $1 - the name of the feature var e.g. 'GIT'
+#       $2 - the 'friendly' name of the feature var e.g. 'install git?'
 function ask_feature {
   local -n ref=$1
   read -n 1 -p "$2 [y/N] " answer
@@ -61,15 +66,21 @@ function ask_feature {
   esac
 }
 
+# purpose: output the value of a feature variable
+# args: $1 - the name of the feature var e.g. 'GIT'
+#       $2 - the 'friendly' name of the feature var e.g. 'install git'
 function print_feature_selection {
   case "${!1}" in
-    [yY]* ) printf "${GREEN}[Run ]${RST} $2\n"
+    [yY]* ) printf "${GREEN}[Run ]${NOCOLOR} $2\n"
             ;;
-    [nN]* ) printf "${RED}[Skip]${RST} $2\n"
+    [nN]* ) printf "${RED}[Skip]${NOCOLOR} $2\n"
             ;;
   esac
 }
 
+# purpose: call print_feature_selection on all feature variables
+# args: none
+#
 function print_feature_selections {
   print_feature_selection GIT "Install GIT"
   print_feature_selection GIT_PROMPT "Add git branch into bash prompt"
@@ -85,16 +96,18 @@ function print_feature_selections {
 }
 
 
+# purpose: clear the terminal window and print out the init.sh header
+# args: none
 function print_header {
   clear
-  printf "${RST}"
+  printf "${ORANGE}"
   printf "===============================================================================\n"
   printf "init.sh | hostname: $HOSTNAME | user: $USERNAME | date: $DATE \n"
   printf "===============================================================================\n"
-  printf "\n"
+  printf "\n${NOCOLOR}"
 }
 
-# INSTALL FUNCTIONS
+############## INSTALL FUNCTIONS ##############
 
 function install_git {
   apt install git
@@ -105,40 +118,48 @@ function set_git_prompt {
   \ngit branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
   \n}" >> ~/.bashrc
 
-  echo "$PS1=$PS1\$(parse_git_branch)" >> ~/.basrc
+  echo "$PS1=$PS1\$(parse_git_branch)" >> ~/.bashrc
 
 }
 
-print_header
-read -n 1 -s -r -p  "Hello $USERNAME! Welcome to init. Press any key to begin"
+############ END INSTALL FUNCTIONS ############
 
-# ask about each available option
-print_header
+# purpose: runs the main script
+# args: none
+function main {
 
-printf "${NC}"
-ask_feature GIT "Install GIT?"
-ask_feature GIT_PROMPT "Add git branch into bash prompt?"
-ask_feature PYENV "Install pyenv?"
-ask_feature PYTHON3 "Install Python 3 (via pyenv)"
-ask_feature PYTHON2 "Install Python 2 (via pyenv)"
-ask_feature BASH_PROMPT "Set custom bash prompt?"
-ask_feature UPDATE_EXISTING "Upgrade all installed packages?"
-ask_feature BUILD_TOOLS "Install build essential tools (cmake, make, c++ c, etc)?"
-ask_feature YARN "Install yarn?"
-ask_feature NPM "Install NPM?"
-ask_feature NODEJS "Install NodeJS?"
-printf "${RST}"
+  print_header
+  read -n 1 -s -r -p  "Hello $USERNAME! Welcome to init. Press any key to begin"
 
-# review selected options
-print_header
-print_feature_selections
+  # ask about each available option
+  print_header
 
-printf "\n"
+  ask_feature GIT "Install GIT?"
+  ask_feature GIT_PROMPT "Add git branch into bash prompt?"
+  ask_feature PYENV "Install pyenv?"
+  ask_feature PYTHON3 "Install Python 3 (via pyenv)"
+  ask_feature PYTHON2 "Install Python 2 (via pyenv)"
+  ask_feature BASH_PROMPT "Set custom bash prompt?"
+  ask_feature UPDATE_EXISTING "Upgrade all installed packages?"
+  ask_feature BUILD_TOOLS "Install build essential tools (cmake, make, c++ c, etc)?"
+  ask_feature YARN "Install yarn?"
+  ask_feature NPM "Install NPM?"
+  ask_feature NODEJS "Install NodeJS?"
 
-read -n 1 -p "Continue with these options? [y/N] " answer
-case $answer in
-  [yY]* ) ;;
-      * ) clear
-	  exit
-	  ;;
-esac
+  # review selected options
+  print_header
+  print_feature_selections
+
+  printf "\n"
+
+  read -n 1 -p "Continue with these options? [y/N] " answer
+  case $answer in
+    [yY]* ) ;;
+        * ) clear
+      exit
+      ;;
+  esac
+}
+
+# run the main function
+main
